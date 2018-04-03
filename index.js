515c514cead21d01dc775f1e842ee8a99940b284
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 21:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var musicians=['Paul McCartney', 'John Lennon', 'George Harrison', 'Ringo Starr']
 var instruments=['Bass Guitar', 'Guitar', 'Lead Guitar', 'Drums']
-
+var new_array=[]
 
 function theBeatlesPlay(musicians, instruments){
-  var new_array = [];
-}for (var new_array; musicians; instruments) {
+}for (new_array; musicians; instruments) {
   new_array.push(`${musicians} plays ${instruments}`)
 }console.log(new_array)
