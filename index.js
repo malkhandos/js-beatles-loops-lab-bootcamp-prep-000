ebdd44d00f1893df563c7001ec0c76be5917f347
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 21:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var musicians=['Paul McCartney', 'John Lennon', 'George Harrison', 'Ringo Starr']
-var instruments=['Bass Guitar', 'Guitar', 'Lead Guitar', 'Drums']
-var new_array=[]
+var musicians=['Paul McCartney', 'John Lennon', 'George Harrison', 'Ringo Starr'];
+var instruments=['Bass Guitar', 'Guitar', 'Lead Guitar', 'Drums'];
+var new_array=[];
 
 function theBeatlesPlay(musicians, instruments){
 }for (var new_array; musicians[0]; instruments[0]) {
   new_array.push(`${musicians} plays ${instruments}`)
 }console.log(new_array)
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
