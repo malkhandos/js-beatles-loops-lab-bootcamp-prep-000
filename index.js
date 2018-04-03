43fd4393c3b620bc272524f8f6b2e9fd8d80233d
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 21:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var instruments=['Bass Guitar', 'Guitar', 'Lead Guitar', 'Drums']
 
 function theBeatlesPlay(musicians, instruments){
   var new_array = [];
-}for (new_array; musicians; instruments) {
+}for (var new_array; musicians; instruments) {
   new_array.push(`${musicians} plays ${instruments}`)
 }console.log(new_array)
