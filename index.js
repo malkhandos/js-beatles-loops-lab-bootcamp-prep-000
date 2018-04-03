428a4e1fa1fb821585c28b467bddad30215395f0
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 21:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var musicians=['Paul McCartney', 'John Lennon', 'George Harrison', 'Ringo Starr']
 var instruments=['Bass Guitar', 'Guitar', 'Lead Guitar', 'Drums']
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+
 
 function theBeatlesPlay(musicians, instruments){
   var new_array = [];
