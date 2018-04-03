0918574e9b420e4f179c6ecc93f17c27cd70e4f5
Rommy Elyan <relyan@email.arizona.edu>
2018-04-03 22:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(n){
   var beatles = [];
   do {beatles.push('I love the Beatles!')}
-  while (beatles.length>7)
+  while (beatles.length<8)
   
 return beatles}
